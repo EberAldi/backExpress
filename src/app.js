17ef371013e49374cdd234a43d28f1d371334b23
Aldair <ebernachito@gmail.com>
2026-04-17 13:28:39 -0600

    login

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import { AppDataSource } from "./data-source.js";
-
+import cors from 'cors'
 import userRoutes from "./user/user.controller.js";
 import authRoutes from "./auth/auth.controller.js";
 
@@ -14,11 +14,13 @@ AppDataSource.initialize()
   .then(() => {
     console.log("DB conectada");
 
+    app.use(cors())
     app.use("/api", userRoutes);
     app.use("/api", authRoutes);
 
     app.listen(PORT, () => {
       console.log(`Servidor en http://localhost:${PORT}`);
     });
+    
   })
   .catch(console.error);
